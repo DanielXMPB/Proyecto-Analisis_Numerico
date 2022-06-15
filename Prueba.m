cc722e758d453bfcbc22edb5af3b2e77ba310d4a
LRC25 <107116345+LRC25@users.noreply.github.com>
2022-06-15 09:09:04 -0500

    Update Prueba.m

--- Prueba.m
+++ Prueba.m
@@ -9,9 +9,9 @@ save('Zm.mat','Zm')
 imagesc(Zm(:,:,198));
 Zr = Rd(Z,R);
 matfile('Zr.mat','Writable',true);
-save('Zr.mat','Zm')
+save('Zr.mat','Zr')
 figure,imagesc(Zr(:,:,3));
 Zb = Bd(Z,B);
 matfile('Zb.mat','Writable',true);
-save('Zb.mat','Zm')
+save('Zb.mat','Zb')
 figure,imagesc(abs(fftshift(Zb(:,:,198))));
